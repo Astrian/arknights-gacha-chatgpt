c36ad1bff09592501cb8226f96d5bb74c7c86a8d
Astrian Zheng <happy.wheel6743@astrian.moe>
2023-05-13 20:07:08 +1000

    first commit

---
+++ app.ts
@@ -0,0 +1,46 @@
+import Debug from 'debug'
+import koa from 'koa'
+import koaBody from 'koa-body'
+import route from 'koa-route'
+import serve from 'koa-static-server'
+import cors from '@koa/cors'
+import axios from 'axios'
+
+const print = Debug('agc:app')
+
+const app = new koa()
+app.use(koaBody())
+
+app.use(cors({
+  origin: 'https://chat.openai.com',
+}))
+
+app.use(serve({rootDir: '.well-known', rootPath: '/.well-known'}))
+
+app.use(route.get('/gacha', async ctx => {
+  let gachaList: Gacha[] = []
+  for (let i = 1; i <= 3; i++){
+    const res = await axios.get(`https://ak.hypergryph.com/user/api/inquiry/gacha?page=${i}&token=oeUWMmPVUd1vEgK1g8D4xRql&channelId=1`)
+    gachaList = gachaList.concat(res.data.data.list)
+  }
+  for (let i in gachaList) {
+    for (let j in gachaList[i].chars) {
+      gachaList[i].chars[j].rarity = gachaList[i].chars[j].rarity + 1
+    }
+  }
+  print(ctx.request)
+  ctx.header['content-type'] = 'application/json'
+  ctx.body = gachaList
+}))
+
+app.listen(3000, () => print('Server is running at http://localhost:3000'))
+
+declare type Gacha = {
+  ts: number
+  pool: string
+  chars: {
+    name: string
+    rarity: number
+    isNew: boolean
+  }[]
+}
