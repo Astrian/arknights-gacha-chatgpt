d398d996afe8fd972e5a6a4c296216b7df6f07c7
Astrian Zheng <happy.wheel6743@astrian.moe>
2023-05-13 10:43:09 +0000

    Use submitted token

--- app.ts
+++ app.ts
@@ -5,8 +5,10 @@ import route from 'koa-route'
 import serve from 'koa-static-server'
 import cors from '@koa/cors'
 import axios from 'axios'
+import dotenv from 'dotenv'
 
 const print = Debug('agc:app')
+dotenv.config()
 
 const app = new koa()
 app.use(koaBody())
@@ -18,9 +20,19 @@ app.use(cors({
 app.use(serve({rootDir: '.well-known', rootPath: '/.well-known'}))
 
 app.use(route.get('/gacha', async ctx => {
+  // Get authentication header
+  const auth = ctx.request.headers.authorization
+  const token = auth?.split(' ')[1]
+  // encode uri
+  const encodedToken = encodeURIComponent(token ?? '')
+
+  print(`Token: ${encodedToken}`)
+
   let gachaList: Gacha[] = []
   for (let i = 1; i <= 3; i++){
-    const res = await axios.get(`https://ak.hypergryph.com/user/api/inquiry/gacha?page=${i}&token=&channelId=1`)
+    
+    const res = await axios.get(`https://ak.hypergryph.com/user/api/inquiry/gacha?page=${i}&token=${encodedToken}&channelId=1`)
+    print(res.data)
     gachaList = gachaList.concat(res.data.data.list)
   }
   for (let i in gachaList) {
@@ -33,7 +45,7 @@ app.use(route.get('/gacha', async ctx => {
   ctx.body = gachaList
 }))
 
-app.listen(3000, () => print('Server is running at http://localhost:3000'))
+app.listen(process.env.PORT ?? 3000, () => print(`Server running on port ${process.env.PORT ?? 3000}`))
 
 declare type Gacha = {
   ts: number
